aebe27a61fe6546152b679f12bb4398efb828a50
夜影、NightShadow <169712528+1224HuangJin@users.noreply.github.com>
2026-04-30 16:30:23 +0200

    Change date format to use slashes
    Updated date display format to include slashes between year, month, and day.

--- src/views/Func/index.vue
+++ src/views/Func/index.vue
@@ -12,9 +12,9 @@
         <div class="right cards">
           <div class="time">
             <div class="date">
-              <span>{{ currentTime.year }}&nbsp;años&nbsp;</span>
-              <span>{{ currentTime.month }}&nbsp;mes&nbsp;</span>
-              <span>{{ currentTime.day }}&nbsp;dias&nbsp;</span>
+              <span>{{ currentTime.year }}&nbsp; / &nbsp;</span>
+              <span>{{ currentTime.month }}&nbsp; / &nbsp;</span>
+              <span>{{ currentTime.day }}&nbsp; / &nbsp;</span>
               <span class="sm-hidden">{{ currentTime.weekday }}</span>
             </div>
             <div class="text">
